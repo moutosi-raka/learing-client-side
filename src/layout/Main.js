a85dbad19660a6597b4346a81ed283b3870dc9c1
moutosi-raka <raka29077997@gmail.com>
2022-10-25 20:44:58 +0600

    set up main.js file

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer";
+import Header from "../pages/Shared/Header";
+import LeftSideNav from "../pages/Shared/LeftSideNav";
 
 const Main = () => {
-    return (
+  return (
+    <div>
+      <Header></Header>
+      <div className="grid lg:grid-cols-3 grid-cols-1 gap-4">
         <div>
-            <Outlet></Outlet>
+          <LeftSideNav></LeftSideNav>
         </div>
-    );
+        <div className="col-span-2">
+          <Outlet></Outlet>
+        </div>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
