256b5833616f59a222ef2832a9a6f40d15a8b594
moutosi-raka <raka29077997@gmail.com>
2022-10-26 12:29:29 +0600

    adde instruction cart in course details

--- src/pages/Courses/CourseCart/CourseCart.js
+++ src/pages/Courses/CourseCart/CourseCart.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { FaArrowRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const CourseCart = ({ course }) => {
-  const { title, amount, image_url} = course;
+  const {category_id, title, amount, image_url} = course;
   return (
     <div>
       <div className="card card-compact w-96 bg-base-100 shadow-xl ">
@@ -14,9 +15,11 @@ const CourseCart = ({ course }) => {
           <hr/>
           <div className="card-actions justify-between">
           <h2 className=" card-title text-black">${amount}</h2>
-          <button className="btn btn-outline w-1/2"> 
+          <Link to={`/course-details/${category_id}`} >
+          <button className="btn btn-outline w-full">
             See Details <FaArrowRight className="ml-3"></FaArrowRight>
           </button>
+          </Link>
           </div>
         </div>
       </div>
--- src/pages/CoursesDetails/CoursesDetails.js
+++ src/pages/CoursesDetails/CoursesDetails.js
@@ -1,13 +1,41 @@
-import React from 'react';
-
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+import { FaCheck } from "react-icons/fa";
 
 const CoursesDetails = () => {
+  const { info, amount, title, instructor, image_url, details, video } =
+    useLoaderData();
 
-    return (
+  return (
+    <div>
+      <h1 className="text-center text-3xl pt-8">Welcome, {title} Course</h1>
+      <div class="grid lg:grid-cols-2 grid-cols-1 gap-4 mt-12">
         <div>
-            <h1>this is courses details</h1>
+          <h2 className="text-2xl mb-5">Course Instructor</h2>
+          <div className="card  card-side bg-slate-900 shadow-xl h-24 ">
+            <figure>
+              <img
+                className="h-20 rounded-full p-2 mx-5"
+                src={instructor.img}
+                alt="Movie"
+              />
+            </figure>
+            <div className="card-body  p-0">
+              <h2 className="card-title pt-2">{instructor.name}</h2>
+              <p className="text-xs">{instructor.study}</p>
+            </div>
+          </div>
+          <div>
+          <h2 className="text-2xl mb-5 mt-8">What you will learn by doing the course</h2>
+          {
+            details.map(d => <div className="flex"><FaCheck className="text-2xl mr-3" ></FaCheck><p>{d}</p></div>)
+          }
+          </div>
         </div>
-    );
+        <div>09</div>
+      </div>
+    </div>
+  );
 };
 
 export default CoursesDetails;
